e1b468022adf2f05f21c5c0f0dc0295898f421a3
Rafael Nicolas <97722548+FaelN1@users.noreply.github.com>
2023-02-28 15:20:04 -0300

    Add files via upload

--- src/app/utils/sender.ts
+++ src/app/utils/sender.ts
@@ -1,70 +1,78 @@
-import parsePhoneNumber, { isValidPhoneNumber } from "libphonenumber-js"
-import { create, Whatsapp, SocketState } from "venom-bot"
+import parsePhoneNumber, { isValidPhoneNumber } from "libphonenumber-js";
+import { create, Whatsapp, SocketState } from "venom-bot";
 
 export type QRCode = {
-    base64Qr: string
-}
+  base64Qr: string;
+};
 
 class Sender {
+  private client: Whatsapp;
+  private connected: boolean;
+  private qr: QRCode;
 
-    private client: Whatsapp
-    private connected: boolean;
-    private qr: QRCode;
+  get isConnected(): boolean {
+    return this.connected;
+  }
 
-    get isConnected(): boolean {
-        return this.connected
+  get qrCode(): QRCode {
+    return this.qr;
+  }
+
+  constructor() {
+    this.initialize();
+  }
+
+  async sendText(to: string, body: string) {
+    if (!isValidPhoneNumber(to, "BR")) {
+      throw new Error("Numero Invalido");
     }
 
-    get qrCode(): QRCode {
-        return this.qr
-    }
+    let phoneNumber = parsePhoneNumber(to, "BR")
+      ?.format("E.164")
+      ?.replace("+", "") as string;
 
-    constructor() {
-        this.initialize()
-    }
+    phoneNumber = phoneNumber.includes("@c.us")
+      ? phoneNumber
+      : `${phoneNumber}@c.us`;
 
-    async sendText(to: string, body: string) {
+    await this.client.sendText(phoneNumber, body);
+  }
 
-        if (!isValidPhoneNumber(to, "BR")) {
-            throw new Error("Numero Invalido")
-        }
+  async leaveGroup(groupId: string) {
 
-        let phoneNumber = parsePhoneNumber(to, "BR")
-            ?.format("E.164")
-            ?.replace("+", "") as string
+    let group = groupId.includes("@g.us")
+      ? groupId
+      : `${groupId}@g.us`;
 
-        phoneNumber = phoneNumber.includes("@c.us")
-            ? phoneNumber
-            : `${phoneNumber}@c.us`
+    await this.client.leaveGroup(group);
+  }
 
-        await this.client.sendText(phoneNumber, body)
-    }
+  private initialize() {
+    const qr = (base64Qr: string) => {
+      this.qr = { base64Qr };
+    };
 
-    private initialize() {
+    const status = (statusSession: string) => {
+      this.connected = ["isLogged", "qrReadSuccess", "chatsAvailable"].includes(
+        statusSession
+      );
+    };
 
-        const qr = (base64Qr: string) => { this.qr = { base64Qr } }
+    const start = (client: Whatsapp) => {
+      this.client = client;
+      client.onStateChange((state) => {
+        this.connected = state === SocketState.CONNECTED;
+      });
+      client.onMessage((message) => {
+        client.sendSeen(message.from);
+      });
+    };
 
-        const status = (statusSession: string) => {
-            this.connected = ["isLogged", "qrReadSuccess", "chatsAvailable"].includes(statusSession)
-        }
-
-        const start = (client: Whatsapp) => {
-            this.client = client
-            client.onStateChange((state) => {
-                this.connected = state === SocketState.CONNECTED
-            })
-            client.onMessage((message) => {
-                
-                client
-                    .sendText(message.from, 'Bot on')
-                }
-
-            );
-        }
-
-        //create("session"+Date.now(), qr).then((client) => start(client)).catch((error) => console.error(error))
-        create('Client', qr).then((client) => start(client)).catch((error) => console.error(error))
-    }
+    //create("session"+Date.now(), qr).then((client) => start(client)).catch((error) => console.error(error))
+    create("Client", qr)
+      .then((client) => start(client))
+      .catch((error) => console.error(error));
+  }
 }
 
-export default Sender
+export default Sender;
